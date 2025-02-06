db1c564a1639485e2949a6cf672735c284e26b1b
Alex Newcomer <alexmnewcomer@gmail.com>
2025-02-06 18:22:30 -0500

    updated the candidate.interface.tsx

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,11 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 export interface Candidate {
-  id: number;
-  name: string;
-  email: string;
-  phone: string;
-  skills: string[];
-  experience: number;
-  education: string;
-  avatar: string;
+    name: string;
+    username: string;
+    location: string;
+    avatar: string;
+    email: string;
+    html_url: string;
+    company: string;
 }
