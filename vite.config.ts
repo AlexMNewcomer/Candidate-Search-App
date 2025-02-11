d4bba4d4cd0161a1c8dd57012fb69a6d692d1b18
Alex Newcomer <alexmnewcomer@gmail.com>
2025-02-11 11:29:22 -0500

    Updated vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
+// Load environment variables from .env
+dotenv.config();
+
 export default defineConfig({
-  envDir: './env',
   plugins: [react()],
+  define: {
+    "process.env.VITE_GITHUB_TOKEN": JSON.stringify(process.env.VITE_GITHUB_TOKEN),
+  },
 });
